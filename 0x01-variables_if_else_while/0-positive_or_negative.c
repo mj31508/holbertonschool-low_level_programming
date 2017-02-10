e36bb9bce612244a20b33e182d75153efb6b592c
Max <mj31508@gmail.com>
2017-02-10 02:09:28 -0500

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - generates a random integer and prints a brief description
+ * Return: integer plus classification (pos/neg/zero)
  */
 int main(void)
 {
@@ -14,17 +13,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positve\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-
 	}
 	else
 	{
 		printf("%d is negative\n", n);
-
 	}
 	return (0);
 }
