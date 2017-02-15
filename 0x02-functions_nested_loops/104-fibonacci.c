18a1d3858be038ab6ee2bdde37ccced186a2fef1
Max <mj31508@gmail.com>
2017-02-15 02:20:14 -0500

    fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- * main - fibonacci numbers.
+ * main - print a table of the first 178 fibonacci numbers.
  * Return: Nothing.
  */
 int main(void)
 {
 	int count;
 	unsigned long i, j, k;
-	unsigned long ia, ib, ja, jb, ka, kb, take;
-	unsigned long total;
+	unsigned long ia, ib, ja, jb, ka, kb, carry;
+	unsigned long amount;
 
 	count = 0;
 	i = 0;
 	j = 1;
-	amount = 10000000000000000000;
+	amount = 10000000000000000000;/*10^18*/
 	for (count = 1; count <= 91; count++)
 	{
 		k = i + j;
@@ -21,15 +21,15 @@ int main(void)
 		j = k;
 		printf("%d %lu\n", count + 1, k);
 	}
-	ia = i / total;
-	ib = i % total;
-	ja = j / total;
-	jb = j % total;
+	ia = i / amount;
+	ib = i % amount;
+	ja = j / amount;
+	jb = j % amount;
 	while (count <= 178)
 	{
-		take= (ib + jb) / total;
-		ka = (ia + ja) + take;
-		kb = (ib + jb ) - take * total;
+		carry = (ib + jb) / amount;
+		ka = (ia + ja) + carry;
+		kb = (ib + jb ) - carry * amount;
 		ia = ja;
 		ib = jb;
 		ja = ka;
