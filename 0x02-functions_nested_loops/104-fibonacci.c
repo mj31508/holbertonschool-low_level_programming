5505be20bae8ee370c7a46d2d18ad5ab8ff55230
Max <mj31508@gmail.com>
2017-02-15 02:38:25 -0500

    another one

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,80 +1,28 @@
 #include <stdio.h>
-#include "holberton.h"
 
 /**
- * main - prints first 97 fibonacci numbers
+ * main - Print first 50 Fibonacci numbers
  *
- * Description: At the 89th number, switches to uses two
- * variables, to hold first handful of digits and last digits.
- *
- * Return: returns zero
+ * Return: Return 0
  */
 int main(void)
 {
-	unsigned long int i, l, f;
-	int ii, ll, ff, c;
+	long int f, s, sum, tot;
 
-	i = c = 1;
-	l = 2;
-	f = 3;
-
-	printf("%lu, %lu, ", i, l);
-	for (c = 0; c < 95; c++)
+	sum = 0;
+	f = 0;
+	s = 1;
+	tot = 0;
+	while (sum < 4000000)
 	{
-		if (c >= 88)
+		sum = f + s;
+		if (sum % 2 == 0)
 		{
-			ff += chop(f, 18);
-			f %= _llpow(10, 18);
-			printf("%d", ff);
-			ii = ll;
-			ll = ff;
-			ff = ii + ll;
+			tot += sum;
 		}
-		printf("%lu", f);
-		if (c != 94)
-			printf(", ");
-		i = l;
-		l = f;
-		f = l + i;
+		f = s;
+		s = sum;
 	}
-	printf("\n");
+	printf("%ld\n", tot);
 	return (0);
-}
-
-
-/**
- * chop - Cuts integer down to specific decimal place.
- *
- * Description: Divides num by a specific power of ten.
- * @num: number to chop down
- * @times: times to chop it down
- * Return: Returns digits left after division.
- */
-unsigned long int chop(unsigned long int num, int times)
-{
-	return (num / _llpow(10, times));
-}
-
-
-/**
- * _llpow - Quick pow implementation.
- *
- * Description: Multiples number by itself a number of times
- * designated by the exponent.
- *
- * @base: base number
- * @exponent: exponent, number of times to multiply base by itself
- * Return: returns zero
- */
-unsigned long int _llpow(int base, int exponent)
-{
-	unsigned long int val = 1;
-
-	while (exponent > 0)
-	{
-		val *= base;
-		exponent--;
-	}
-	return (val);
-
 }
