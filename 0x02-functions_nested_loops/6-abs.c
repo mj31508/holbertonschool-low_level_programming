5d9df7503238d13200f52fb05c653bf890722981
Max <mj31508@gmail.com>
2017-02-14 23:01:24 -0500

    i need to relearn math

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_abs - computing the absoulute value of a integer
+ *@int - the parameter being passed
+ *@i: int argument that is being passed
+ *Return: Always 0.
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+		return (i);
+}
