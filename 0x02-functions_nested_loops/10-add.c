ad2cfabc32d729239435a8af81aded71949048d2
Max <mj31508@gmail.com>
2017-02-15 02:01:42 -0500

    both

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - adds 2 numbers.
+ * @n:  number to be added.
+ * @i: The second number to be added.
+ * Return: the total.
+ */
+int add(int n, int i)
+{
+	return (n + i);
+}
