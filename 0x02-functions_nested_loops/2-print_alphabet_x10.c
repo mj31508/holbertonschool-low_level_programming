16097e2d75a950cc5f9e793fc5ba1df2fb8287f8
Max <mj31508@gmail.com>
2017-02-14 20:29:27 -0500

    2print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * main - void prototype
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	int temp = 0;
+
+	while (temp <= 9)
+
+	{
+		a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+
+		}
+
+		temp++;
+		_putchar('\n');
+	}
+}
