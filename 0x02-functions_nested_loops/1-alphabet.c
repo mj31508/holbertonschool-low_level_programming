9481e61fe08b57f39076fe01a6ea895e177649cc
Max <mj31508@gmail.com>
2017-02-14 20:26:50 -0500

    1alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,7 @@
  * Return: Always 0.
  */
 
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	char a = 'a';
 
@@ -17,5 +17,4 @@ void print_alphabet (void)
 		a++;
 	}
 	_putchar('\n');
-	
 }
