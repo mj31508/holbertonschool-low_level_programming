6f958223e9dc3de460625e16c4932f147505fbc0
Max <mj31508@gmail.com>
2017-02-16 17:54:31 -0500

    1

--- 0x03-more_functions_nested_loops/1-isdigit.c
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * int _isdigit - Function prototype to check for digits
+ * _isdigit - Function prototype to check for digits
  *@c: variable being used to check the range of 0 and 9
  *
  * Return: 1 if variable is a digit or zero if it isn't
