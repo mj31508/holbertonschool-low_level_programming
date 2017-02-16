80104fefbda9e4f3089cc870f6c29ecdeddd7ffa
Max <mj31508@gmail.com>
2017-02-16 18:08:15 -0500

    multiply

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - multiply two integers
+ *@a: first parameter
+ *@b: second parameter
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
