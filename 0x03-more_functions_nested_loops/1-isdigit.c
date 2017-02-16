698a050bdbc1a444fffe29211b7514c2ba674a74
Max <mj31508@gmail.com>
2017-02-16 17:38:47 -0500

    missing star

--- 0x03-more_functions_nested_loops/1-isdigit.c
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -3,8 +3,9 @@
 /**
  * int _isdigit - Function prototype to check for digits
  *@c: variable being used to check the range of 0 and 9
+ *
  * Return: 1 if variable is a digit or zero if it isn't
- */
+ **/
 
 int _isdigit(int c)
 {
