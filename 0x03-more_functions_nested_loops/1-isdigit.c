b95a15183cd66f54cb3ccfceb22f20dfe9650559
Max <mj31508@gmail.com>
2017-02-16 17:35:39 -0500

    brackets maybe

--- 0x03-more_functions_nested_loops/1-isdigit.c
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,19 @@
 
 /**
  * int _isdigit - Function prototype to check for digits
- *@c: variable being ued to check the range of 0 and 9
+ *@c: variable being used to check the range of 0 and 9
  * Return: 1 if variable is a digit or zero if it isn't
  */
 
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 
 }
