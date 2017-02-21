52143145ab8722797e4a51fbc65c8996784cbcbb
Max <mj31508@gmail.com>
2017-02-21 13:52:04 -0500

    first commit

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - prototye to update the n parameter to 98.
+ *@n: variable to be updated
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
