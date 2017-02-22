32bdd1b2110f7bd86fca0e4887d5b8acd509e5d3
Max <mj31508@gmail.com>
2017-02-22 01:24:43 -0500

    reverse

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_rev - function prototype that holds a pointer variable.
+ *@s: pointer variable
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+
+		i++;
+	}
+
+	i--;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i = i - 1]);
+
+
+	}
+	_putchar ('\n');
+}
