1a10d4e5577095b55dca0561c6d20bd4d331a275
Max <mj31508@gmail.com>
2017-02-23 13:31:24 -0500

    all

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * strncat - holds two pointers and a n variable.
+ *@dest: character pointer
+ *@src: source pointer
+ *@n: n variable to itereate with src
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	return(dest);
+}
