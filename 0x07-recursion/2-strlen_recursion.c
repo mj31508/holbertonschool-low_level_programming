9a732e9a59fad76f80931c30f366c7a5a07f0ad3
Max <mj31508@gmail.com>
2017-03-02 22:50:01 -0500

    commit for 2

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -1,12 +1,18 @@
+#include "holberton.h"
+
 /**
- * _strlen_recursion - length of a string.
- * @s: the string to count
- * Return: length of the string
+ * _strlen_recursion - Function prototype the returns the length of a string
+ *@s: variable pointer
+ * Return: Always 0.
  */
+
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		return (0);
-	s++;
-	return (1 + _strlen_recursion(s));
+	}
+	return (_strlen_recursion(s + 1) + 1);
+
+
 }
