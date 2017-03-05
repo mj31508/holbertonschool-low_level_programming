74fd015a5fb34168395a1426967368d4c2bf8952
Max <mj31508@gmail.com>
2017-03-04 19:42:58 -0500

    1st commit

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+/**
+ * main - function for argument count and vector
+ * @argc: argument count that isn't used
+ * @argv: argument vector
+ * Return: 0
+ ***/
 
 int main (int argc, char *argv[])
 {
 	while(argc--)
 	{
 		printf("%s\n", *argv++);
+		return (0);
 	}
 }
