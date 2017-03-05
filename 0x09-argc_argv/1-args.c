a290f6ac9fba6cbc94955b95dbd8ad48ff7a02e6
Max <mj31508@gmail.com>
2017-03-04 19:22:52 -0500

    first commit

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc,char __attribute__ ((unused)) *argv[])
+/**
+ * main - funtion that holds the argument count
+ * @argc: counts the total num of arguments
+ * @argv: amount of arguments supplied
+ * Return: 0
+ ***/
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
 {
 
 
 		printf("%d\n", argc - 1);
+		return (0);
 }
