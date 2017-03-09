d8cc7641ccf128ad9cc9210f4f61f7d776d73154
Max <mj31508@gmail.com>
2017-03-09 02:57:05 -0500

    tried

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stlib.h>
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int n;
+	int **array;
+
+	if (height < 1 || width < 1)
+		return (0);
+	array=malloc(sizeof
+
+	while (i <= height)
+	{
+		n = 0;
+		while (n <= width)
+		{
+			n = width;
+			n++;
+		}
+		i = height;
+		i++;
+	}
+
+}
