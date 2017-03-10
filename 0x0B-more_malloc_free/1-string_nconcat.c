587d35b91b9342deda3ea665b71d01bee2cc7d8b
Max <mj31508@gmail.com>
2017-03-09 23:39:48 -0500

    2nd commit

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 #include <stdlib.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * string_nconcat - concatenating two strings
+ * @s1: character variable that we will get the length of
+ * @s2: character variable that we will get the length of
+ * @n: unsigned int that we will compare our string length with
+ * Return: Pointer.
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1;
@@ -19,41 +19,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
-	for(len1 = 0; s1[len1] != '\0'; len1++)
+	for (len1 = 0; s1[len1] != '\0'; len1++)
 	{
 	}
-	for(len2 = 0; s2[len2] != '\0'; len2++)
+	for (len2 = 0; s2[len2] != '\0'; len2++)
 	{
 	}
-	len2 = len2 +1;
+	len2 = len2 + 1;
 
 	if (n <= len2)
 	{
 		len2 = n;
-	}
-	ptr = malloc((len1 + len2) * sizeof(char));
-
+	} ptr = malloc((len1 + len2) * sizeof(char));
 	if (ptr == NULL)
-	{
 		return (NULL);
-			}
-
-			for ( i = 0; i < len1; i++)
-			{
-				ptr[i] = s1[i];
-			}
-		for (j = 0; j < len2; j++)
-		{
-			ptr[i] = s2[j];
-			i++;
-		}
-		ptr[i] = '\0';
-		return (ptr);
-
+	for (i = 0; i < len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (j = 0; j < len2; j++)
+	{
+		ptr[i] = s2[j];
+		i++; }
+	ptr[i] = '\0';
+	return (ptr);
 }
