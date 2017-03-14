9f21adab566fe191550a7209c9486fd264c75635
Max <mj31508@gmail.com>
2017-03-13 21:14:26 -0400

    first commit

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+int _putchar(char c);
+
+/**
+ * struct dog - a structure defined by the name dog
+ * @type: access argument
+ * @age: access argument
+ * @owner: access argument
+ **/
+
+typedef struct dog
+{
+	char *type;
+	float age;
+	char *owner;
+}
+
+#endif
