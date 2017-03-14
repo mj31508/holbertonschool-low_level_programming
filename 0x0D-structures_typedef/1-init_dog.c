952f658cfbe006ff6298e7bdf1a4da323f9eb68c
Max <mj31508@gmail.com>
2017-03-13 22:50:48 -0400

    recommit

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,16 +1,19 @@
-#include "DOG_H"
+#include "dog.h"
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ * init_dog - function that holds variable pointers with a void type
+ * @d: pointer character that is at the address of dog
+ * @name: pointer that points to a string
+ * @age: float
+ * @owner: pointer variable that points to the character address
  **/
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
-	struct dog
+	while (d != NULL)
 	{
-		int dog;
-	}struct dog my_dog;
+		d->name = name;
+		d->age = age;
+		d-> owner = owner;
+	}
