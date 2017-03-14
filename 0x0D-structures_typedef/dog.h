390e24696ecdc70cde422fdef3151e129fdf5c5d
Max <mj31508@gmail.com>
2017-03-14 00:24:45 -0400

    quick

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -8,13 +8,15 @@
  * @owner: access argument
  **/
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-dog;
+};
+typedef struct dog dog_t;
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
