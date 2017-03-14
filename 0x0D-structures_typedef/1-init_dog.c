8ac2f49f76f524937fc0a2898d06fc8b45ffc136
Max <mj31508@gmail.com>
2017-03-13 22:46:02 -0400

    1

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "DOG_H"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ **/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+	struct dog
+	{
+		int dog;
+	}struct dog my_dog;
