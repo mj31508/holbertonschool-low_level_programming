40463c43e821b6e957af0fadbdec8c83b6cdb2ec
Max <mj31508@gmail.com>
2017-03-14 22:32:52 -0400

    last commit

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -22,4 +22,5 @@ char *_strcpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 #endif
