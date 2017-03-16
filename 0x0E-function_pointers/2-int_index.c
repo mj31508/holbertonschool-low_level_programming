54432265e06cb7f56b90290ae9c92c96bfe544b1
Max <mj31508@gmail.com>
2017-03-16 18:25:16 -0400

    Returning a double pointer to an array

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+
+/**
+ * int_index - function that holds a function pointer
+ * @array: array that holds the numerical elements
+ * @size: comparison integer gor our array
+ * @cmp: double pointer
+ *
+ *
+ * Return: return -1 if elements match or return size 0
+ ***/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size < 0 || !(array) || !(cmp))
+		return (-1);
+	while (!(cmp(array[i])))
+	{
+		i++;
+		if (i == size)
+			return (-i);
+	}
+	return (i);
+
+}
