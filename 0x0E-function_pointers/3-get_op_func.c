a5ea75e4181e743334a0e59b7dc69e53ee9135a4
Max <mj31508@gmail.com>
2017-03-17 00:17:24 -0400

    recommitting

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,121 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - prototype function that holds a *s pointer
+ * @s: pointer to a character
+ *
+ *
+ * Return: Null if operators aren;t selected.
+ **/
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i =0;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+
+
+
+	while (ops[i]->op != NULL)
+	{
+		if(ops.op[0] == s[0] && s[i] == '\0')
+			return (ops[i]->f);
+		i++;
+	}
+	return (NULL);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "_3_CALC_H"
+
+/**
+ * get_op_func - function that holds a function pointer
+ * @s: charactr pointer
+ *
+ * Return: pointer
+ **/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "_3_CALC_H"
+
+/**
+ * op_add - function to add to integers
+ * @a: first variable for addition
+ * @b: second variable for addition
+ * Return: int
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtraction function
+ * @a: first variable for subtraction
+ * @b: second number for subtracton
+ * Return: int
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+
+}
+
+/**
+ * op_mul - Funtion that muliplies two variables
+ * @a: First variable to be multiplied
+ * @b: Second variable to be multiplied
+ * Return: int
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function that divides two intergers
+ * @a: First int to be divided
+ * @b: Second int to be divided
+ * Return: int
+ **/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function that divides two integers and returns remainder
+ * @a: First integer to be divided
+ * @b: Second integer to be divided
+ * Return: int
+ **/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
