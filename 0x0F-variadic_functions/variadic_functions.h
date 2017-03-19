033045431bd9cb5393d82dd36056851505925936
Max <mj31508@gmail.com>
2017-03-19 19:37:28 -0400

    Committing header file for files on github repository

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -2,15 +2,22 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
-typedef struct s_t {
-
-	char *input;
-	void (*f)();
-}s;
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+
+typedef struct s_t {
+	char *input;
+	void (*f)();
+}s;
+
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
+
+
+
 #endif
