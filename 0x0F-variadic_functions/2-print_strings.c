c2e0ee7af27f15596bf02f2f1f206cfd26b3eed9
Max <mj31508@gmail.com>
2017-03-20 01:12:46 -0400

    2-print_strings.c

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -17,11 +17,11 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i = 0;
+
 	va_list ap;
 
 	va_start(ap, n);
-
-	unsigned int i = 0;
 
 	for (i = 0; i < n; i++)
 	{
