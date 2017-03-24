3a280959fe2de51a2b6f71250e50a41bdec6d6f1
Max <mj31508@gmail.com>
2017-03-24 17:10:02 -0400

    header file

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -6,6 +6,10 @@
 #include <string.h>
 #include <stdarg.h>
 
+int _putchar(char c);
+size_t print_list(const list *h);
+size_t list_len(const list_t *h);
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
