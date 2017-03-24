301262c333fa9c18039e289e25446e4e37a98c8c
Max <mj31508@gmail.com>
2017-03-24 17:07:48 -0400

    adding 1 to git

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "listS_h"
+#include <string.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * list_len - function of type size t
+ * @h: pointer that points at the head of a struct
+ *
+ *
+ * Return: num of elements
+ **/
+
+size_t list_len(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
