5ee5055959165d6c62705fe823e31ab1fc6e9c00
Max <mj31508@gmail.com>
2017-03-23 23:36:10 -0400

    first commit

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+/**
+ * print_list -  function prototype of size_t that holds a head pointer
+ * @h: head pointer in the holberton struct
+ *
+ * Return: Number of nodes
+ **/
+
+size_t print_list(const list_t *h)
+{
+	const struct list_s *temp;
+	unsigned int i;
+	i = 0;
+	temp = h;
+
+	while (temp != NULL)
+	{
+		printf("[%d} %s\n", temp->len, temp->str);
+		temp = temp->next;
+		i++;
+	}
+	return(temp);
+}
