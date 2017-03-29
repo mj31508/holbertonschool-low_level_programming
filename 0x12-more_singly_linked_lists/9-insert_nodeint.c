d32dc10df29581ce648ce2e0b16703fa25eef452
Max <mj31508@gmail.com>
2017-03-29 17:58:35 -0400

    inserting a new node at any given element

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function protype that holds double head pointer and two int
+ * @head: Double pointer that points to struct and head
+ * @n: variable that will be used in new node
+ * @idx: index that is inserted into new node
+ * Return: listint_t
+ **/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *mv_node;
+	unsigned int count;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	mv_node = *head;
+
+	count = 0;
+	while (count != idx - 1)
+	{
+		mv_node = mv_node->next;
+		count++;
+	}
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = mv_node->next;
+	mv_node->next = new_node;
+	return (new_node);
+}
