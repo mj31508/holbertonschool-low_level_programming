2e5d76e09733ee07f5cdccdad7a68768fedea487
Max <mj31508@gmail.com>
2017-03-31 02:26:36 -0400

    tired

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * binary_to_uint - function that holds a constant character
@@ -12,21 +11,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i;
+	unsigned int btoi;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
-	i = 0;
-	while (b[i] != '\0')
+	btoi = 0;
+	while (*b)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		btoi = btoi * 2;
+		if (*b == '1')
+			btoi += 1;
+		else if (*b != '0')
 			return (0);
-		else if (b[i] == '0' && b[i] == '1')
-		{
-			return ((b[i] % 2) + 10 * (b[i] / 2));
-
-		}
-		i++;
+		b++;
 	}
-	return (i);
+	return (btoi);
 }
