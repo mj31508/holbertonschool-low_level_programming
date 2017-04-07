7d09ed590cfe1d9a487fc848d6a3fab7ff00a615
Max <mj31508@gmail.com>
2017-04-06 21:41:39 -0400

    file fix

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -13,40 +13,36 @@ int main(int argc, char **argv)
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 			exit(97);
-
 	open_from = open(argv[1], O_RDONLY);
 	if (open_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			argv[1]), exit(98);
-
 	open_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (open_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
 			 argv[2]), exit(99);
-
 	fd_read = read(open_from, Buffer, BUFF_SIZE);
 	if (fd_read == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			 argv[1]), exit(98);
-
 	while (fd_read > 0)
 	{
 		fd_write = write(open_to, Buffer, fd_read);
 			if (fd_write == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
 				argv[2]), exit(99);
-
-		fd_read = read(open_from, Buffer, BUFF_SIZE);
+			fd_read = read(open_from, Buffer, BUFF_SIZE);
 		if (fd_read == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file
+ s\n", argv[1]), exit(98);
 	}
 	close_from = close(open_from);
 	if (close_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", open_from);
-	exit (100);
+	exit(100);
 	close_to = close(open_to);
 	if (close_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", open_from);
-	exit (100);
+	exit(100);
 	return (0);
 }
