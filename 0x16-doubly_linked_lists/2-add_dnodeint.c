107a8cb73e5f313f86c577f2e0aa6d25be68d853
Max <mj31508@gmail.com>
2017-05-09 00:04:25 -0400

    fix

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -6,15 +6,15 @@
  * @n: const int
  * Return: address of the new element of NULL if failed
  **/
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-        dlistint_t *new_node;
+	dlistint_t *new_node;
+
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 
 		new_node->n = n;
