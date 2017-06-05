90c531b01c7c10ee0317f00438c154b465a0466e
Max <mj31508@gmail.com>
2017-06-05 16:37:25 -0400

    changing the function

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -7,15 +7,13 @@
  **/
 unsigned long int hash_djb2(const unsigned char *str)
 {
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
 	{
-		unsigned long hash = 5381;
-		int c;
-
-		while ((c = *str++))
-		{
-			hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-		}
-
-		return (hash);
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
+	return (hash);
 }
