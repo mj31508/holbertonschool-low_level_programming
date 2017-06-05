7554c5f87afd0027a9f55f6bfae3219bb07707df
Max <mj31508@gmail.com>
2017-06-05 17:19:01 -0400

    update

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+/**
+ * key_index - holds key and size
+ * @key: key
+ * @size: size
+ *
+ * Return: index
+ **/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+        unsigned long int bucket;
+	unsigned long int index;
+
+	bucket = hash_djb2(key);
+	index = bucket % size;
+
+	return (index);
+}
