ee457f5528df19db154ce9db5ba5fc9f6bfa8a03
Max <mj31508@gmail.com>
2017-06-05 17:24:26 -0400

    fix'

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -6,9 +6,10 @@
  *
  * Return: index
  **/
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        unsigned long int bucket;
+	unsigned long int bucket;
 	unsigned long int index;
 
 	bucket = hash_djb2(key);
