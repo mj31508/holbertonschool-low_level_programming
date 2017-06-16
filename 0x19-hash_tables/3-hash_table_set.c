c01dddab843d9241dbcfafe42fe6b4d307c2a7c0
Max <mj31508@gmail.com>
2017-06-16 17:21:18 -0400

    fix

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,21 +14,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 
 	if (strlen(value) == 0)
-		return NULL;
-
+		return (NULL);
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-
 	new_node = malloc(sizeof(hash_node_t));
-
 	if (new_node == NULL)
 	{
 		return (0);
 	}
-
 	index = key_index((unsigned char *)key, ht->size);
-
-
 	new_node->key = strdup(key);
 	new_node->value =  strdup(value);
 	new_node->next =  NULL;
@@ -37,31 +31,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		return (0);
 	}
-
-
 	ht->array[index] = new_node;
-
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new_node;
 	}
-
 	tmp = ht->array[index];
 	while (tmp != NULL)
 		if (strcmp(key, tmp->key) == 0)
 		{
 			free(tmp->value);
-			tmp->value=strdup(value);
+			tmp->value = strdup(value);
 			free(node->key);
 			free(node->value);
 			free(node);
 			return (1);
 		}
-	        tmp = tmp->next;
-
-        }
-	ht->array = new_node;
-        node->next = tmp;
-    }
-    return (1);
+	tmp = tmp->next;
+}
+ht->array = new_node;
+node->next = tmp;
+}
+return (1);
 }
