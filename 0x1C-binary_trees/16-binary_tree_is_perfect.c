c2142e4ebae76502925f939e961611c8912f54e5
Max <mj31508@gmail.com>
2017-07-28 22:06:20 -0400

    fix

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -12,18 +12,20 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int left;
 	int right;
 
-	if (tree != NULL)
+	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL & tree->right != NULL)
+
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+
 	if (binary_tree_balance(tree) != 0)
 		return (0);
+
 	if (tree->left && tree->right)
 	{
 		left = binary_tree_is_perfect(tree->left);
 		right = binary_tree_is_perfect(tree->right);
 		return (left && right);
 	}
-
 	return (0);
 }
