a796bf875236117ba7801c4a948a5dd686412191
Max <mj31508@gmail.com>
2017-07-28 21:14:58 -0400

    chris's code

--- 0x1C-binary_trees/11-binary_tree_size.c
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -10,15 +10,25 @@
  * Return: total
  **/
 
-size_t binary_tree_depth(const binary_tree_t *node)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
+
 	if (tree == NULL)
+	{
 		return (0);
+	}
 	if (tree->left == NULL && tree->right == NULL)
+	{
 		return (1);
+	}
 	if (tree->left != NULL && tree->right == NULL)
+	{
 		return (2);
+	}
 	if (tree->left == NULL && tree->right != NULL)
+	{
 		return (2);
-	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+	}
+	/* Add 1 to count root node */
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
