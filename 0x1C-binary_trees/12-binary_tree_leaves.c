6da69b0c2d4708bb2d7d747e230c44ccc7521849
Max <mj31508@gmail.com>
2017-07-28 21:16:44 -0400

    fix

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -12,9 +12,9 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree != NULL)
+	if (tree == NULL)
 		return (0);
-        if (tree->left != NULL && tree->right !=NULL)
+        if (tree->left == NULL && tree->right ==NULL)
 		return (1);
 	return (binary_tree_leaves(tree->right + binary_tree_leaves(tree->left));
 }
