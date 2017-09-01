672bd0a4e57244b26957ff05dff5ac3215e35902
Max <mj31508@gmail.com>
2017-09-01 01:00:30 -0400

    my first linear search

---
+++ 0x1D-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in array of ints
+ * @array: pointer to a int thats the first element of a array
+ * @size: size of an array
+ * @value: value to search for in array
+ * Return: 
+ **/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	i = 0;
+
+	while (i < size)
+	{
+		printf("Value checked array[%d] = [%d]", (int)i, array[i]);
+		if (array[i] == value)
+			return (i);
+		else if (array[i] != value)
+			i++;
+	}
+	return (-1);
+}
