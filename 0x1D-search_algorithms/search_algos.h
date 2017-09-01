c0bb547a3f1333419430d2d6ed21edbcf7a171e4
Max <mj31508@gmail.com>
2017-09-01 01:31:53 -0400

    all

---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+#include <stdio.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void p_array(int *array, size_t position, size_t size);
+
+#endif
